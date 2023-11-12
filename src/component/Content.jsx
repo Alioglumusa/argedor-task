3f368a392af6194306715a1ac1c6e527a19b9580
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-11-12 23:00:54 +0300

    fixed responsive sidebar

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -3,13 +3,17 @@ import DownloadBar from "./DownloadBar";
 import WorkSpace from "./WorkSpace";
 import Tweets from "./Tweets";
 
-const Content = ({open}) => {
+const Content = ({ open }) => {
   return (
-  <div className={`${open ? "ml-[350px]" : "ml-[70px]" } absolute top-20 transition-all md:w-[96%] w-[300px]`}>
-  <DownloadBar/>
-  <WorkSpace/>
-  <Tweets/>
-  </div>
+    <div
+      className={`${
+        open ? "ml-[350px]" : "ml-[70px]"
+      } absolute top-20 transition-all  `}
+    >
+      <DownloadBar />
+      <WorkSpace />
+      <Tweets />
+    </div>
   );
 };
 
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -87,7 +87,7 @@ const Sidebar = ({ open, pin, setOpen, setPin }) => {
   };
 
   return (
-    <div onMouseOver={screenWidth > 760 ? expandSidebar : null} onMouseOut={collapseSidebar} className="h-screen md:relative fixed">
+    <div onMouseOver={screenWidth > 760 ? expandSidebar : null} onMouseOut={collapseSidebar} className="h-screen md:relative fixed mobileFixed">
       {/* //Header Blue Side */}
       <div
         className={` ${
@@ -123,7 +123,7 @@ const Sidebar = ({ open, pin, setOpen, setPin }) => {
       <div
         className={` ${
           open ? "w-[350px]" : "w-[68px] "
-        }  bg-black  transition-all md:h-[1360px] h-screen lg:h-[639px] `}
+        }  bg-black  transition-all md:h-[1360px] h-[900px] lg:h-[639px] `}
       >
         <div className="flex justify-center ">
           <img
